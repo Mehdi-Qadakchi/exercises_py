8cbd860ca55c1823bec21368694d968fbafff488
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-15 03:19:59 +0430

    slove number 2 from exercise 1

---
+++ prj1/exe_2.py
@@ -0,0 +1,17 @@
+num_1 = input('Enter first number : ')
+num_2 = input('Enter second number : ')
+operator = input('Enter your operator (+ - * /) : ')
+
+if operator == '+':
+    print(float(num_1) + float(num_2))
+elif operator == '-':
+    print(float(num_1) - float(num_2))
+elif operator == '*':
+    print(float(num_1) * float(num_2))
+elif operator == '/':
+    if float(num_2) == 0.0:
+        print('error zero devision')
+    else:
+        print(float(num_1) / float(num_2))
+else:
+    print('Please enter one of these operators * - + /')
