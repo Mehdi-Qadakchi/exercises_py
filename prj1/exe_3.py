885a30ba95b444414121605220ea21bc4eea8ea3
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-15 16:53:16 +0430

    slove number  3 from exercise 1

---
+++ prj1/exe_3.py
@@ -0,0 +1,6 @@
+x = float(input('Enter edge x from rectangle : '))
+y = float(input('Enter edge y from rectangle : '))
+
+area = x * y
+diameter = x + y
+print(f'x : {x}, y : {y}\narea = {area}\ndiameter = {diameter}')
