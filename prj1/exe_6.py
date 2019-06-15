f354381077c4569865dadbdfaae0a3cc3f64116a
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-15 18:10:46 +0430

    slove number 6 from exercise 1

---
+++ prj1/exe_6.py
@@ -0,0 +1,4 @@
+temp_in_c = float(input('Enter your temprature in celcius : '))
+temp_in_far = (temp_in_c * 9/5) + 32
+
+print(f'temprature in celcius is {temp_in_c}\ntemprature in fahrenheit is {temp_in_far}')
