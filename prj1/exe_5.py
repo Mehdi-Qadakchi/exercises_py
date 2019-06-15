578703bae3c3a5184d150cbb34a936171ad0555d
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-15 17:51:06 +0430

    solve number 5 from exercise 1

---
+++ prj1/exe_5.py
@@ -0,0 +1,3 @@
+stature_in_cm = float(input('Enter your stature in centimeter : '))
+stature_in_inch = stature_in_cm * 0.393701
+print(f'stature in centimeter is {stature_in_cm}\nstature in inch is {stature_in_inch}')
