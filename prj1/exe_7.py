49fd86813475e18fbe6aa3d6d299c2411452e6a3
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-15 18:31:33 +0430

    slove number 7 from exercise 1

---
+++ prj1/exe_7.py
@@ -0,0 +1,6 @@
+age = int(input('enter your age : '))
+age_in_mounth = age * 12
+age_in_week = age_in_mounth * 4
+age_in_day = age_in_week * 7
+
+print(f'your age is {age}\nyou pass {age_in_mounth} mounths\nyou pass {age_in_week} weeks\nyou pass {age_in_day} day')
