25e3177bcb0358c94ff0ef439ac6296dbc8b838b
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-15 19:26:55 +0430

    solve number 11 from exercise 1

---
+++ prj1/exe_11.py
@@ -0,0 +1,5 @@
+student_name = input('enter your name : ')
+student_id = input('enter student id : ')
+student_phone = input('enter student cellphone : ')
+
+print(f'name\t:\t{student_name}\nid\t:\t{student_id}\nphone\t:\t{student_phone}')
