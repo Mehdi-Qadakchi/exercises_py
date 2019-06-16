2102fe9d36cde7573ab830e1860ce515ac6ff975
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-16 13:06:39 +0430

    solve number 3 from exercise 2

---
+++ prj2/exe_3.py
@@ -0,0 +1,19 @@
+num_1 = float(input('enter first number : '))
+num_2 = float(input('enter second number : '))
+num_3 = float(input('enter third number : '))
+
+if num_1 < num_2 and num_1 < num_3:
+    if num_2 < num_3:
+        print(num_1, num_2, num_3)
+    else:
+        print(num_1, num_3, num_2)
+elif num_2 < num_1 and num_2 < num_3:
+    if num_1 < num_3:
+        print(num_2, num_1, num_3)
+    else:
+        print(num_2, num_3, num_1)
+elif num_3 < num_1 and num_3 < num_2:
+    if num_1 < num_2:
+        print(num_3, num_1, num_2)
+    else:
+        print(num_3, num_2, num_1)
