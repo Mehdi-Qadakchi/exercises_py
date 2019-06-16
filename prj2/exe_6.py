18253aa875e67944469432c31218bbf9faac86b5
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-16 17:50:51 +0430

    solve number 6 from exercise 2

---
+++ prj2/exe_6.py
@@ -0,0 +1,7 @@
+num_1 = float(input('enter first number : '))
+num_2 = float(input('enter second number : '))
+
+if num_1 == num_2:
+    print('both number are equal.')
+else:
+    print(f'first number is {num_1} and second number is {num_2}')
