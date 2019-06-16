923cf9980f7ec82dceb4797f238b2d45b9beabc9
Mehdi Qadakchi <offensive.2019@gmail.com>
2019-06-16 17:05:14 +0430

    solve number 5 from exercise 2

---
+++ prj2/exe_5.py
@@ -0,0 +1,16 @@
+food_list = ['Hot Dog', 'Pizza', 'Hamburger', 'Bread', 'Cheese']
+
+print('''
+1.\tHot Dog
+2.\tPizza
+3.\tHamburger
+4.\tBread
+5.\tCheese
+''')
+food_id = int(input('enter your food id : '))
+
+
+if food_id in range(1, len(food_list)+1):
+    print(food_list[food_id - 1])
+else:
+    print('\nthis id is not in food list')
