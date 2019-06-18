2e1321ba55e83ead8e1e3b57cd56eb303101057b
Saeed Gholami <saeed.ghollami@gmail.com>
2019-06-18 12:40:32 +0430

    Change formula of perimeter of the rectangle
    Perimeter of a rectangle calculated by this formula:
    (width  + height) * 2

--- prj1/exe_3.py
+++ prj1/exe_3.py
@@ -4,5 +4,5 @@ x = float(input('Enter edge x from rectangle : '))
 y = float(input('Enter edge y from rectangle : '))
 
 area = x * y
-diameter = x + y
+perimeter = (x + y) * 2
 print(f'x : {x}, y : {y}\narea = {area}\ndiameter = {diameter}')
