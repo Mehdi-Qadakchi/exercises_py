64be70eb2e3d6cc8a0293f66119d3e8268bf7663
Saeed Gholami <saeed.ghollami@gmail.com>
2019-06-20 00:28:09 +0430

    don't change sequence value in the body loop
    num used in range function and you decrease it in the body of loop which bad idea and possibly make the program crashed.

--- prj3/exe_4.py
+++ prj3/exe_4.py
@@ -2,8 +2,7 @@ num = int(input('enter a number for looping : '))
 total = 0
 
 for i in range(num):
-    x = int(input('enter a number ({}): '.format(num)))
-    num -= 1
-    total += x
+    n = int(input(f'Enter a number ({i}): '))
+    total += n
 
 print(total)
